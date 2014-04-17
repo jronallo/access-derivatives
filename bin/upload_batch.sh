e1e303a69386245bc7fa471aeec83748a69f34cb
Jason Ronallo <jronallo@gmail.com>
2014-04-17 09:25:03 -0400

    first attempt at batch upload script

---
+++ bin/upload_batch.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# upload_batch.sh /path/to/batch/directory
+
+# Setting for directory to where the batch directory ought to be uploaded
+upload_batch_directory_base=/var/uploads/
+
+# This only works if the local user
+whoami=`whoami`
+
+# Grab the first argument into a variable so that we can do the basename substitution.
+argv0=$1
+# The basename from whatever path was given
+basename=${argv0##*/}
+
+# Copy the files to the remote server under the upload batch directory.
+scp -r $1 $whoami@av1.lib.ncsu.edu:$upload_batch_directory_base
+
+# Trigger the processing job on the remove server and background it using nohup.
+# Output and errors will go into files. FFmpeg output goes to standard error.
+ssh $whoami@av1.lib.ncsu.edu "nohup process_video /var/uploads/$basename > upload_batch.out 2> upload_batch.err < /dev/null &"

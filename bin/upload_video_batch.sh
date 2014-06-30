9d4bb84eb4fad69a7e1af4f6ffad31023cd7d48a
Jason Ronallo <jronallo@gmail.com>
2014-06-30 09:57:00 -0400

    change for new upload location with more space

--- bin/upload_video_batch.sh
+++ bin/upload_video_batch.sh
@@ -3,7 +3,7 @@
 # upload_video_batch.sh /path/to/batch/directory
 
 # Setting for directory to where the batch directory ought to be uploaded
-upload_batch_directory_base=/var/uploads
+upload_batch_directory_base=/html5/video/uploads
 
 # This only works if the local user
 whoami=`whoami`
@@ -22,11 +22,11 @@ echo -e 'Copying the files to av1... (May need to enter password).\n'
 scp -r $1 $whoami@av1.lib.ncsu.edu:$upload_batch_directory_base
 
 echo -e 'Changing group permissions of files. (May need to enter password).'
-ssh $whoami@av1.lib.ncsu.edu "chmod g+rx $full_upload_path && chmod g+rw $full_upload_path_contents"
+ssh $whoami@av1.lib.ncsu.edu "chgrp uploaders $full_upload_path && chmod g+rx $full_upload_path && chmod g+rw $full_upload_path_contents"
 
 echo "Hit ENTER when you wish to continue with processing the video. (May need to enter password)."
 read SOMETHIGNANYTHING
 
 # Trigger the processing job on the remove server and background it using nohup.
 # Output and errors will go into files. FFmpeg output goes to standard error.
-ssh $whoami@av1.lib.ncsu.edu "nohup process_video $full_upload_path > upload_batch.out 2> upload_batch.err < /dev/null &"
+ssh $whoami@av1.lib.ncsu.edu "PROCESS_VIDEO_OUTPUT_DIRECTORY=/html5/video/output nohup process_video $full_upload_path > upload_batch.out 2> upload_batch.err < /dev/null &"
